68ffa555431622a6c205fa3b3b30588dd6e1e22d
R cqls <rdrouilh@gmail.com>
2022-08-30 01:56:03 +0200

    Update try.v

--- examples/try.v
+++ examples/try.v
@@ -10,13 +10,15 @@ fn main() {
 	world.new_entity("tutu")
 	world.new_entity("titi")
 
-	vecs.comp1(world).set("titi",vecs.Comp1{})
-	vecs.comp2(world).set("titi",vecs.Comp2{})
+	mut comp1 := vecs.comp1(world)
+	comp1.set("titi",vecs.Comp1{})
+	mut comp2 := vecs.comp2(world)
+	comp2.set("titi",vecs.Comp2{})
 
-	vecs.comp1(world).set("tutu",vecs.Comp1{gx.rgb(10,10,10)})
+	comp1.set("tutu",vecs.Comp1{gx.rgb(10,10,10)})
 	
-	mut c1 := vecs.comp1(world).get("titi") ?
+	mut c1 := comp1.get("titi") ?
 	c1.color = gx.rgb(20,0,0)
-	vecs.comp1(world).set("titi",c1)
-	println(vecs.comp1(world).get("titi") ?)
+	comp1.set("titi",c1)
+	println(comp1.get("titi") ?)
 }
